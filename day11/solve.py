db3cde8c5bf8b41841ca115c98510a7eb3fed87c
David Russell <dave.a.russell@gmail.com>
2023-04-03 12:59:04 +0100

    day 11 puzzle

---
+++ day11/solve.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import sys
+
+
+def main(input_file):
+    data = open(input_file).read().strip()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
+
+
+# cqjxjnds
+
+# cqjxxyzz
+
+# cqkaabcc
